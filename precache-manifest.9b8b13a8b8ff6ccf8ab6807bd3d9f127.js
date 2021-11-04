5700d23e4edb09a29924a35f63d66faff3f283ec
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-11-03 21:17:27 -0600

    Updates

---
+++ precache-manifest.9b8b13a8b8ff6ccf8ab6807bd3d9f127.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9564862d789aa4b5990f51b9fedf7d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e3bcd21a4f0924afad",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "3a3997428e6b4d354f1a",
+    "url": "/static/css/main.fb5e79b7.chunk.css"
+  },
+  {
+    "revision": "76e3bcd21a4f0924afad",
+    "url": "/static/js/2.a0957df9.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/static/js/2.a0957df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5977ae6858945c27dd1",
+    "url": "/static/js/3.16196fe3.chunk.js"
+  },
+  {
+    "revision": "3a3997428e6b4d354f1a",
+    "url": "/static/js/main.bc0d8b37.chunk.js"
+  },
+  {
+    "revision": "eb1f997605c6a7ee4ba0",
+    "url": "/static/js/runtime-main.34321489.js"
+  },
+  {
+    "revision": "71de07266ad51a426e2302999c7de805",
+    "url": "/static/media/eats-n-such.71de0726.png"
+  },
+  {
+    "revision": "6d1dbd9013faad8cc8b3aa0908389870",
+    "url": "/static/media/food-festival.6d1dbd90.jpg"
+  },
+  {
+    "revision": "8320e20171e6b2fe0775a8db33bd8750",
+    "url": "/static/media/landmark_tracker.8320e201.jpg"
+  },
+  {
+    "revision": "1a5a341263f35a6d313703e05d1791cc",
+    "url": "/static/media/mysql.1a5a3412.png"
+  },
+  {
+    "revision": "e272217ac68ad4eb31a7056942df1937",
+    "url": "/static/media/onlymemes.e272217a.jpg"
+  },
+  {
+    "revision": "e857b301928a72cf5e7cfc378dc9fd14",
+    "url": "/static/media/portfolio.e857b301.jpg"
+  },
+  {
+    "revision": "de272e7d463f1568d733301b15e06460",
+    "url": "/static/media/react.de272e7d.png"
+  },
+  {
+    "revision": "4c9070be70f52f58e84f49299146c63c",
+    "url": "/static/media/readme-generator.4c9070be.jpg"
+  },
+  {
+    "revision": "62f2b2db9c2a0969d416034795abd39f",
+    "url": "/static/media/run-buddy.62f2b2db.png"
+  }
+]);
